d8fdc52699a4ac566572c58234ab9a5121a40386
MukundKulkarni <mukundkulkarni786@gmail.com>
2019-06-07 15:17:15 +0530

    Changed settings

--- Enotes/settings.py
+++ Enotes/settings.py
@@ -122,11 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+CRISPY_TEMPLATE_PACK ='bootstrap4'
 
 
-STATICFILES_DIRS = [
-	os.path.join(BASE_DIR, 'static'),
-]
 
 LOGIN_REDIRECT_URL = '/user-home/'
 LOGOUT_REDIRECT_URL = '/'
